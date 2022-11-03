f8ca2d6dbfa5befb9cbaabc9d14860341a670b69
cailynkit <cailynkitchens8@gmail.com>
2022-11-03 14:02:13 -0400

    11/3/2022

---
+++ index.js
@@ -0,0 +1,8 @@
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"), //class
+  point: document.querySelector("point"),
+  empty: document.querySelectorAll(".nothing"),
+};
+
+console.log(DOMSelectors.point);
