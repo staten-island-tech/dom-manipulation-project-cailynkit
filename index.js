6ff5aaa62994f98ddbf2dfd5b8be6a77bfa12dec
cailynkit <cailynkitchens8@gmail.com>
2022-11-14 14:02:53 -0500

    11/14/22

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
-  text: document.querySelector("#text"), //class
-  box: document.getElementById("big-black-box"),
-  points: document.querySelectorAll(".point"),
-  background: document.querySelector("#big-black-box"),
+  song: document.getElementById("song"),
+  results: document.getElementById("rusults"),
 };
 
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.innerHTML = "this is a red box";
-  text.style.fontSize = "40px";
-}
-
 DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+  DOMSelectors.results.innerHTML = `
+  <h1>Howdy </h1>
+  Hello there! You like ${DOMSelectors.song.value}
+  `;
 });
