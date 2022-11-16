57704f9a2dc193c64515c27b8a24bba538ee0178
cailynkit <cailynkitchens8@gmail.com>
2022-11-16 14:00:48 -0500

    11/16/22

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
+  enter: document.getElementById("btn1"),
+  remove: document.getElementById("btn2"),
   song: document.getElementById("song"),
   artist: document.getElementById("artist"),
   results: document.getElementById("results"),
@@ -10,8 +11,12 @@ const user = {
   artist: "",
 };
 
-DOMSelectors.button.addEventListener("click", function () {
+DOMSelectors.enter.addEventListener("click", function () {
   user.song = DOMSelectors.song.value;
   user.artist = DOMSelectors.artist.value;
   DOMSelectors.results.innerHTML = `<br /> Hi! You like the song ${user.song} by the artist ${user.artist}.`;
+});
+
+DOMSelectors.remove.addEventListener("click", function () {
+  DOMSelectors.results.innerHTML = ``;
 });
