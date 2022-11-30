dd8da95dec6bdc939c82e552742b44f884c9b5c6
cailynkit <cailynkitchens8@gmail.com>
2022-11-30 14:02:11 -0500

    fixed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const DOMSelectors = {
-  enter: document.getElementById("enter"),
-  clear: document.getElementById("clear"),
   song: document.getElementById("song"),
   artist: document.getElementById("artist"),
+  enter: document.getElementById("enter"),
+  clear: document.getElementById("clear"),
   results: document.getElementById("results"),
-  div: document.getElementById("container"),
+  div: document.getElementById("div"),
+  innerHTML: document.getElementById("innerHTML"),
+  innerHTMLButton: document.getElementById("innerHTMLButton"),
 };
 
 const user = {
@@ -12,17 +14,30 @@ const user = {
   artist: "",
 };
 
-DOMSelectors.enter.addEventListener("click", function () {
+DOMSelectors.enter.addEventListener("click", function (song, artist, html) {
   user.song = DOMSelectors.song.value;
   user.artist = DOMSelectors.artist.value;
   DOMSelectors.div.insertAdjacentHTML(
     "beforeend",
-    `<p>You like the song ${user.song} by the artist ${user.artist}.</p>`
+    `<p id="innerHTML">You like the song ${user.song} by the artist ${user.artist}.</p>
+    <button type="button" id="innerHTMLButton">Remove</button>`
   );
+  DOMSelectors.song.value = "";
+  DOMSelectors.artist.value = "";
+
+  let innerHTMLButton = document.getElementById("innerHTMLButton");
+  let innerHTML = document.getElementById("innerHTML");
+  innerHTMLButton.addEventListener("click", function () {
+    innerHTML.remove();
+    innerHTMLButton.remove();
+  });
 });
 
 DOMSelectors.clear.addEventListener("click", function () {
   DOMSelectors.song.value = "";
   DOMSelectors.artist.value = "";
-  DOMSelectors.div.insertAdjacentHTML.remove();
+  let innerHTML = document.getElementById("innerHTML");
+  innerHTML.remove();
+  let innerHTMLButton = document.getElementById("innerHTMLButton");
+  innerHTMLButton.remove();
 });
